7ad56389036415eafc4ce698f503452d73a5ed5b
Nahide <gelgecnahide@gmail.com>
2020-04-28 15:26:45 -0400

    add addCommit.java

---
+++ src/day_01/addCommand.java
@@ -0,0 +1,9 @@
+package day_01;
+
+public class addCommand {
+    public static void main(String[] args) {
+        //will see you in Github
+
+        //I made changes here when I created
+    }
+}
