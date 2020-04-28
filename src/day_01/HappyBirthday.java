895c8142c4ee133d7c27dbffae08593c890e2d98
Nahide12 <64107193+Nahide12@users.noreply.github.com>
2020-04-28 16:07:16 -0400

    Update HappyBirthday.java

--- src/day_01/HappyBirthday.java
+++ src/day_01/HappyBirthday.java
@@ -2,7 +2,7 @@ package day_01;
 
 public class HappyBirthday {
     public static void main(String[] args) {
-        System.out,println("Happy Birthday");
+        System.out.println("Happy Birthday");
          
         //dfgh
         ///i made some changes
